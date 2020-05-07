05d709f696ff2c96a7fa958e5567a59e837bc0db
SPECL Browser <exp.pcl.mu@gmail.com>
2020-05-07 09:32:32 -0700

    pushData commit

--- test/local.R
+++ test/local.R
@@ -1,13 +1,20 @@
 #Jeff Rouder, Local Data Extractor
 extractData=function(d,code){
   stim=d$sender=="Stim"
-  ntrials=table(d$observation[stim])
+  sessionID=d$observation[stim]
+  ntrials=table(sessionID)
   codeTrials=rep(code,ntrials)
   targ=d[stim,]$targ
   back=d[stim,]$targ
   morph=d[stim,]$morph
   resp=d[stim,]$response
   rt=d[stim,]$duration
-  out=data.frame(codeTrials,targ,back,morph,resp,rt)
-  colnames(out)=c("code","targ","background","morph","resp","rt")
+  out=data.frame(codeTrials,sessionID,targ,back,morph,resp,rt)
+  colnames(out)=c("participantID",
+                  "sessionID",
+                  "targ",
+                  "background",
+                  "morph",
+                  "resp",
+                  "rt")
   return(out)}
