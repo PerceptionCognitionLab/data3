cd631d24afe4103999f9d70f429e2e4629237d76
SPECL Browser <exp.pcl.mu@gmail.com>
2020-05-27 20:09:22 -0700

    pushData commit

---
+++ bright2/local.R
@@ -0,0 +1,20 @@
+extractData=function(d,pid){
+  stim=d$sender=="Stim"
+  respEvent=d$sender=="Resp"
+  sessionID=d$observation[stim]
+  ntrials=table(sessionID)
+  participantID=rep(pid,ntrials)
+  targ=d[stim,]$targ
+  back=d[stim,]$targ
+  morph=d[stim,]$morph
+  resp=d[respEvent,]$response
+  rt=d[respEvent,]$duration
+  out=data.frame(participantID,sessionID,targ,back,morph,resp,rt)
+  colnames(out)=c("participantID",
+                  "sessionID",
+                  "targ",
+                  "background",
+                  "morph",
+                  "resp",
+                  "rt")
+  return(out)}
