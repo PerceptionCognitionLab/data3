1e4fe5c811a0c578a46b9023923d0ce3f6f9dc53
SPECL Browser <exp.pcl.mu@gmail.com>
2020-05-28 13:51:37 -0700

    pushData commit

--- bright2/local.R
+++ bright2/local.R
@@ -1,18 +1,35 @@
 extractData=function(d,pid){
   stim=d$sender=="Stim"
   respEvent=d$sender=="Resp"
-  sessionID=d$observation[stim]
-  ntrials=table(sessionID)
-  participantID=rep(pid,ntrials)
+  
+  ## Get the participants who actually responded to every stimulus
+  sessionID=d$observation[stim] # stimulus session ID
+  sessionID2 <- d[respEvent,]$observation # response session ID
+  stimrespmatch <- match(table(sessionID), table(sessionID2))
+  goodsessID <- names(table(sessionID)[which(stimrespmatch == 2)])
+  sessionID.common <- sessionID[sessionID %in% goodsessID]
+  
+  # ntrials=table(sessionID)
+  # participantID=rep(pid, each = table(sessionID))
+  
+  # Get stimulus information
   targ=d[stim,]$targ
-  back=d[stim,]$targ
+  back=d[stim,]$back
   morph=d[stim,]$morph
+  stim.df <- data.frame(targ, back, morph)
+  stim.df <- subset(stim.df, sessionID %in% goodsessID)
+  
+  # Get response information
   resp=d[respEvent,]$response
   rt=d[respEvent,]$duration
-  out=data.frame(participantID,sessionID,targ,back,morph,resp,rt)
-  colnames(out)=c("participantID",
+  resp.df <- data.frame(resp, rt)
+  resp.df <- subset(resp.df, sessionID2 %in% goodsessID)
+  
+  out= cbind(sessionID.common, stim.df, resp.df)
+  colnames(out)=c(
+    #"participantID",
                   "sessionID",
-                  "targ",
+                  "target",
                   "background",
                   "morph",
                   "resp",
