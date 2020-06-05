45230969b1177ec83cffe9f986e745b5c2e279de
SPECL Browser <exp.pcl.mu@gmail.com>
2020-06-04 19:19:15 -0700

    pushData commit

--- brightCompare1/local.R
+++ brightCompare1/local.R
@@ -2,10 +2,10 @@ blockFun=function(x) as.integer(x[1])
 trialFun=function(x) as.integer(x[2])
 
 extractData=function(d){
-  resp=d$sender=="Resp"
+  r=d$sender=="Stim"
   dat0=data.frame(d$observation,d$timestamp,d$sender_id,d$targ_level,d$back_level,d$response,round(d$duration))
   colnames(dat0)=c("sid","timestamp","sender_id","targLevel","backLevel","resp","rt")
-  datR=dat0[resp,]
+  datR=dat0[r,]
   good=!duplicated(cbind(datR$timestamp,datR$sid))
   dat=datR[good,]
   index0=strsplit(as.character(dat$sender_id),"_")
