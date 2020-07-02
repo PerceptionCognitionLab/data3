41623d4fb21d0f48dffbf6c8f180b880ef503337
SPECL Browser <exp.pcl.mu@gmail.com>
2020-07-02 09:52:55 -0700

    pushData commit

--- mueller_pilot/local.R
+++ mueller_pilot/local.R
@@ -2,7 +2,7 @@ blockFun=function(x) as.integer(x[1])
 trialFun=function(x) as.integer(x[2])
 
 extractData=function(d){
-  r=d$sender=="Stim"
+  r=d$sender=="Resp"
   dat0=data.frame(d$observation
                   ,d$timestamp
                   ,d$sender_id
@@ -11,12 +11,10 @@ extractData=function(d){
                   ,d$back
                   ,d$response
                   ,round(d$duration)
-                  ,d$correct
-                  , d$total_number_of_correct_responses
-                  , d$arrow_top)
+                  ,d$correct)
   colnames(dat0)=c("sid","timestamp","sender_id", "pid"
                    ,"targ","back","resp", "rt"
-                   ,"correct", "total_correct_responses", "arrow_top")
+                   ,"correct")
   datR=dat0[r,]
   good=!duplicated(cbind(datR$timestamp,datR$sid))
   dat=datR[good,]
