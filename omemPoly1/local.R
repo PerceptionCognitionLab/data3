88f4e0402c3d83bc11928891933062f9434b7802
SPECL Browser <exp.pcl.mu@gmail.com>
2021-04-29 21:02:16 -0700

    pushData commit

---
+++ omemPoly1/local.R
@@ -0,0 +1,17 @@
+extractData=function(d){
+  stim=d$sender=="Screen"
+  respEvent=d$sender=="Screen"
+  sid=d$observation[stim]
+  same=d[stim,]$same
+  diff=d[stim,]$different
+  resp=d[respEvent,]$response
+  correct=d[respEvent,]$correct
+  rt=d[respEvent,]$duration
+  out=data.frame(sid,same,diff,resp,correct,rt)
+  colnames(out)=c("sid",
+		  "same",
+		  "different",
+                  "resp",
+		  "correct",
+                  "rt")
+  return(out)}
